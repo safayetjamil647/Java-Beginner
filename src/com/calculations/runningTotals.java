703bb7a43378f3da01943ab43a575a67aced1c89
safayetjamil647 <work.jamil4@gmail.com>
2020-04-25 15:58:32 +0600

    second commit

--- src/com/calculations/runningTotals.java
+++ src/com/calculations/runningTotals.java
@@ -1,12 +1,14 @@
 package com.calculations;
 
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class runningTotals {
     public static void main(String[] args) {
         int days;
         double sales;
         double totalSales=0.0;//total run
+        DecimalFormat dollar=new DecimalFormat("##0.00");
         String input = JOptionPane.showInputDialog("for how many days do you have sales"+"figures");
         days = Integer.parseInt(input);
         for(int count=1;count<=days;count++)
@@ -15,7 +17,7 @@ public class runningTotals {
             sales=Double.parseDouble(input);
             totalSales+=sales;
         }
-        JOptionPane.showMessageDialog(null,"total sales is"+totalSales);
+        JOptionPane.showMessageDialog(null,"total sales is"+dollar.format(totalSales));
         System.exit(0);
     }
 
--- src/com/calculations/sentinelValues.java
+++ src/com/calculations/sentinelValues.java
@@ -1,4 +1,25 @@
 package com.calculations;
 
+import javax.swing.*;
+
 public class sentinelValues {
+    public static void main(String[] args) {
+        int value;
+        int doubleValue;
+        String input= JOptionPane.showInputDialog("enter a value to double"+"(0to stop)");
+
+        value= Integer.parseInt(input);
+
+        while(value != 0)
+        {
+            doubleValue=value*2;
+            JOptionPane.showMessageDialog(null,""+value+"doubled"+doubleValue);
+
+             input= JOptionPane.showInputDialog("enter a value to double"+"(0to stop)");
+
+            value= Integer.parseInt(input);
+
+        }
+        System.exit(0);
+    }
 }
